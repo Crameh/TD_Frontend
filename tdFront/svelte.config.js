bd0165da9f56f9718691916dc93e279a6d03dc95
Marc HE <he.marc321@gmail.com>
2023-02-16 09:02:52 +0100

    changes for vercel

--- tdFront/svelte.config.js
+++ tdFront/svelte.config.js
@@ -1,11 +1,9 @@
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
-			edge: false,
-			split: false
 		})
 	}
 };
